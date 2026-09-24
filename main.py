d46423ec391acba9aef2296af74fc531071fb271
jamesyjamesman <jameson.price@my.uwrf.edu>
2026-09-23 23:58:17 -0500

    draws more cohesive lines, which supports the mouse being lifted

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ import tkinter as tk
 root = tk.Tk()
 
 canvas = tk.Canvas(root, width=640, height=320)
-canvas.grid(row = 0, column = 0, sticky = tk.NSEW)
+canvas.grid(row = 0, column = 0)
 
 root.minsize(640, 320)
 root.title("AI Calculator")
 
+old_x = None
+old_y = None
+
 def on_mouse_move(event):
+    global old_x
+    global old_y
     x = event.x
     y = event.y
-    print (x, y)
-    canvas.create_rectangle(x, y, x, y, fill="black")
+    old_x = x if old_x is None else old_x
+    old_y = y if old_y is None else old_y
+    canvas.create_line(old_x, old_y, x, y, fill="black")
+    old_x = x
+    old_y = y
+
+def on_mouse_release(event):
+    global old_x
+    global old_y
+    old_x = None
+    old_y = None
 
 canvas.bind("<B1-Motion>", on_mouse_move)
+canvas.bind("<ButtonRelease-1>", on_mouse_release)
 
 root.mainloop()
