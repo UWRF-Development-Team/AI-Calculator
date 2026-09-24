2869fcbd5f00592f2d9dba1ec398d63573119ac9
jamesyjamesman <jameson.price@my.uwrf.edu>
2026-09-24 14:22:07 -0500

    Add button to clear canvas

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def on_mouse_release(event):
 def save_image(event):
     global text_input
     converted_name = convert_symbols(text_input.get()) + ".0"
-    files = Path("./generated-data")
+    files = Path("training-data")
     highest_name = converted_name
     # Kind of messy
     for file in files.iterdir():
@@ -52,7 +52,7 @@ def save_image(event):
         if file.name.split(".", 1)[0] == highest_name.split(".", 1)[0]:
             if int(file.name.split(".", 2)[1]) >= int(highest_name.split(".", 1)[1]):
                 highest_name = file.name.split(".", 1)[0] + "." + str(int(file.name.rsplit(".", 2)[1]) + 1)
-    image.save(f"./generated-data/{highest_name}.png")
+    image.save(f"./training-data/{highest_name}.png")
 
 def convert_symbols(text):
     assert_type(text, str)
@@ -63,6 +63,13 @@ def convert_symbols(text):
             .replace("x", "m")
             )
 
+def clear_canvas(event):
+    global image
+    global draw
+    image = Image.new("RGB", (640, 320), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    canvas.delete("all")
+
 canvas.bind("<B1-Motion>", on_mouse_move)
 canvas.bind("<ButtonRelease-1>", on_mouse_release)
 canvas.bind("<Button-3>", save_image)
@@ -70,5 +77,9 @@ canvas.bind("<Button-3>", save_image)
 button = tk.Button(root, text="Save")
 button.bind("<Button-1>", save_image)
 button.grid(row = 1, column = 1)
+
+clear_button = tk.Button(root, text="Clear")
+clear_button.bind("<Button-1>", clear_canvas)
+clear_button.grid(row = 1, column = 2)
 
 root.mainloop()
